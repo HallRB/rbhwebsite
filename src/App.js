5b917f03ea67a4e93bb00e81472ae77e00ac34fd
HallRB <richardbrianhall@gmail.com>
2023-06-22 11:52:19 -0500

    restart

--- src/App.js
+++ src/App.js
@@ -1,55 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
-import TheatrePage from './TheatrePage';
-
-
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/theatre">Theatre</Link>
-            </li>
-            <li>
-              <Link to="/art">Art</Link>
-            </li>
-            <li>
-              <Link to="/coding">Coding</Link>
-            </li>
-            <li>
-              <Link to="/games">Games</Link>
-            </li>
-            <li>
-              <Link to="/work-experience">Work Experience</Link>
-            </li>
-            <li>
-              <Link to="/about-me">About Me</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-        <header className="App-header">
-          <h1>Welcome to my website</h1>
-          <h2>Richard B. Hall</h2>
-        </header>
-        <main>
-          <Route path="/theatre" component={TheatrePage} />
-          {/* Add other Route components for other pages */}
-          <section id="games">
-            <h3>Games</h3>
-            {/* Add your games content here */}
-          </section>
-        </main>
-        <footer>
-          {/* Add your footer content here */}
-        </footer>
-      </div>
-    </Router>
+    <div className="App">
+      <nav>
+        <ul>
+          <li><a href="#theatre">Theatre</a></li>
+          <li><a href="#art">Art</a></li>
+          <li><a href="#coding">Coding</a></li>
+          <li><a href="#games">Games</a></li>
+          <li><a href="#work-experience">Work Experience</a></li>
+          <li><a href="#about-me">About Me</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+      <header>
+        <h1>Welcome to my website</h1>
+        <h2>Richard B. Hall</h2>
+      </header>
+      <main>
+        {/* Add your content here */}
+      </main>
+      <footer>
+        {/* Add your footer content here */}
+      </footer>
+    </div>
   );
 }
+
+export default App;
